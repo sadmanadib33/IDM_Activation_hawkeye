ffa3ddfbe6f8dac11f0c93f9f24dce9af59bb74b
Sadman Adib <74928568+sadmanadib33@users.noreply.github.com>
2024-10-05 11:54:15 +0600

    Update activate.ps1

--- activate.ps1
+++ activate.ps1
@@ -1,19 +1,39 @@
 # Enable TLSv1.2 for compatibility with older clients
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
-
-$DownloadURL = 'https://naeembolchhi.github.io/IDM-Activator/IDMA-20230031145358.cmd'
-
-$FilePath = "$env:TEMP\IDMA.cmd"
-
-try {
-    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
-} catch {
-    Write-Error $_
-	Return
+if (-not ([System.Net.ServicePointManager]::SecurityProtocol -band [System.Net.SecurityProtocolType]::Tls12)) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 }
 
-if (Test-Path $FilePath) {
-    Start-Process $FilePath -Wait
-    $item = Get-Item -LiteralPath $FilePath
-    $item.Delete()
+$DownloadURL = 'https://sadmanadib33.github.io/IDM_Activation_hawkeye/IDM_Activation.cmd'
+
+# Define the temporary file path
+$FilePath = Join-Path $env:TEMP 'IDMA.cmd'
+
+try {
+    # Download the file
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    # If an error occurs, log it and stop execution
+    Write-Error "Failed to download the file from $DownloadURL. Error: $_"
+    return
+}
+
+# Check if the file was downloaded successfully
+if (Test-Path -Path $FilePath) {
+    try {
+        # Start the process and wait for it to complete
+        Start-Process -FilePath $FilePath -Wait -NoNewWindow
+    } catch {
+        # Handle any errors while starting the process
+        Write-Error "Failed to start the process for $FilePath. Error: $_"
+        return
+    }
+
+    # Remove the downloaded file
+    try {
+        Remove-Item -Path $FilePath -Force
+    } catch {
+        Write-Error "Failed to delete the file $FilePath. Error: $_"
+    }
+} else {
+    Write-Error "The file $FilePath does not exist."
 }
